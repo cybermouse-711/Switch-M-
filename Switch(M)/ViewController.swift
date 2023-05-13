47d7364f549597595320b39858bcb80cf26273cc
ChauveSouris <desseobscurite@gmail.com>
2023-05-13 23:46:53 +0300

    Added metod CGColor

--- Switch(M)/ViewController.swift
+++ Switch(M)/ViewController.swift
@@ -22,20 +22,49 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        colorView.layer.cornerRadius = 20
-        
+        setupView()
     }
 
     // MARK: - IBActions
     @IBAction func changeRedColor() {
+        redColorLabel.text = redColorSlider.value.formatted()
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(bleuColorSlider.value),
+            alpha: CGFloat(0.5)
+        )
     }
     
     @IBAction func changeGreenColor() {
+        greenColorLabel.text = greenColorSlider.value.formatted()
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(bleuColorSlider.value),
+            alpha: CGFloat(0.5)
+        )
     }
     
     @IBAction func changeBlueColor() {
+        blueColorLabel.text = bleuColorSlider.value.formatted()
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(bleuColorSlider.value),
+            alpha: CGFloat(0.5)
+        )
     }
     
+    // MARK: - Private Methods
+    private func setupView() {
+        colorView.layer.cornerRadius = 20
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(0.0),
+            green: CGFloat(0.0),
+            blue: CGFloat(0.0),
+            alpha: CGFloat(0.5)
+        )
+    }
 }
 
